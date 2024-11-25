03d895522f26a8d47f03cef29470c4e346098bb9
Tarek Abdo <tarekk.abdo22@gmail.com>
2024-11-26 01:00:12 +0200

    start working on gameScreen component

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,8 +1,28 @@
-import { Text } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 
 const GameScreen = () => {
   // this shows when the client pick a number
-  return <Text>GameScreen</Text>;
+  return (
+    <View style={styles.screen}>
+      <Text>Opponent's Guess</Text>
+      {/* Guess */}
+      <View>
+        <Text>Higher or Lower?</Text>
+        {/* + - */}
+      </View>
+      {/* <View>LOG ROUNDS</View> */}
+    </View>
+  );
 };
 
 export default GameScreen;
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    padding: 14,
+    // backgroundColor: "#fff",
+    // justifyContent: "center",
+    // alignItems: "center",
+  },
+});
