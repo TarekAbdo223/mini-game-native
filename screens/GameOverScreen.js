d611041c360e9540f404a19d99c2f8e250c9ad15
Tarek Abdo <tarekk.abdo22@gmail.com>
2024-11-30 23:35:50 +0200

    start the styling for the game over screen

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,12 +1,44 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Image, StyleSheet, Text, View } from "react-native";
+import Title from "../components/ui/Title";
+import Colors from "../constatnts/colors";
 
 const GameOverScreen = () => {
   return (
-    <View>
-      <Text>Game Over</Text>
+    <View style={styles.rootContainer}>
+      <Title>Game Over</Title>
+      <View style={styles.imageContainer}>
+        <Image
+          style={styles.image}
+          source={require("../assets/images/success.png")}
+        />
+      </View>
+      <Text>Your Phone Needed X Rounds To Guess The Number Y.</Text>
     </View>
   );
 };
 
 export default GameOverScreen;
+
+const styles = StyleSheet.create({
+  rootContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 24,
+    // backgroundColor: Colors.primary700,
+  },
+  imageContainer: {
+    borderRadius: 150,
+    width: 300,
+    height: 300,
+    overflow: "hidden",
+    borderWidth: 3,
+    borderColor: Colors.primary800,
+    margin: 36,
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+  },
+});
